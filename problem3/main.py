b2c09beb108d63d3c878ee961fcfc0ee5e348a54
Ahza Mazola <ahzamazola@gmail.com>
2024-05-12 02:03:28 +0700

    memasukkan jawaban

--- problem3/main.py
+++ problem3/main.py
@@ -1,6 +1,13 @@
 def join_array_remove_duplicate(arrayA, arrayB):
-    # your code here
-    return []
+    i = []
+    for data in arrayA:
+        if data not in i:
+            i.append(data)
+    for data in arrayB:
+        if data not in i:
+            i.append(data)
+    return i
+    
 
 if __name__ == '__main__':
     # Test cases
